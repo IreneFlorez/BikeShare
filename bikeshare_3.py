c72d0cf0222b7ddfa0dd6a44e14a649d4289866b
Irene <gummyrene@gmail.com>
2018-06-20 13:09:50 -0700

    Update bikeshare_3.py

--- bikeshare_3.py
+++ bikeshare_3.py
@@ -1,145 +1,78 @@
 # Importing packages and functions
-import time
 import pandas as pd
 import numpy as np
 import datetime # operations to parse dates
+import time
+import csv
+
+# supported cities
+cities = {'chicago': 'chicago.csv',
+          'new york': 'new_york_city.csv',
+          'washington': 'washington.csv',}
 
 #get user input for city
 def get_city():
-    try:
-        city = input('\nHello! Let\'s explore some US bikeshare data!\n'
-                     'Would you like to see data for Chicago, New York City, or Washington?\n')
-        # read data and assign to city_data
-        city_data = pd.read_csv("%s.csv" % city.lower().replace(' ', '_'))
-        city_data['Start Time'] = pd.to_datetime(city_data['Start Time'])
-        city_data['End Time'] = pd.to_datetime(city_data['End Time'])
-        city_data['day_of_week'] = city_data['Start Time'].dt.weekday_name
-        city_data['month'] = city_data['Start Time'].dt.strftime('%B')
-        city_data['hour'] = city_data['Start Time'].dt.strftime('%H %p')
-        return city_data
-    except:
-        print('That is not a valid answer. Please try again.')
-        get_city()
+  city_input = input('Hello! Let\'s explore some US bikeshare data!\nWould you like to see data for Chicago, New York, or Washington?\n')
+  if city_input.lower() in cities.keys():
+    return city_input.lower()
+  else:
+    print('That is not a valid answer. Please try again.')
+
+def get_city_data(filename):
+  print('Accessing %s' % filename)
+  return pd.read_csv(filename)
 
 # get time period for filtering
-def get_time_period(city_data):
-        time_period = input('\nWould you like to filter the data by month, day, or not at'
-                        ' all? Type "none" for no time filter.\n').lower()
-        if time_period == 'month' or time_period == 'm':
-            return ['month', get_month()]
-        elif time_period == 'day' or time_period == 'd':
-            return ['day', get_day_of_week()]
-        elif time_period == 'none' or time_period == 'n':
-            return ['none', 'no filter']
-        else:    
-            print('That is not a valid answer. Please try again.')
-        return get_time_period()
+def get_time_period():
+  time_period = input('Would you like to filter the data by month, day, or not at all? Type "none" for no time filter.\n').lower()
+  if time_period == 'month' or time_period == 'm':
+      return 'month'
+  elif time_period == 'day' or time_period == 'd':
+      return 'day'
+  elif time_period == 'none' or time_period == 'n':
+      return 'none'
+  else:    
+      print('That is not a valid answer. Please try again.')
 
 # get month for filtering
-def get_month(city_data):
+def get_month():
     try:
-        month = input('\nWhich month? January, February, March, April, May, or June?\n').lower()
-        return city_data[city_data['Start Time'].dt.month == datetime.strptime(month, '%B').month]
+      return input('January, February, March, April, May, or June?\n').lower()
     except:
-        print('That is not a valid answer. Please try again.')
-        return get_month(city_data) 
+      print('That is not a valid answer. Please try again.')
 
 # get day of week for filtering
-def get_day_of_week(city_data):
+def get_day():
     try:
-        day_of_week = input('\nWhich day of the week? Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday?\n').title()
-        return city_data[city_data['day_of_week'] == day]
+        return input('\nWhich day of the week? Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday?\n').title()
     except:    
         print('That is not a valid answer. Please try again.')
-        return get_day_of_week()
-
-def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
-
-    print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()
-
-    # display the most common month
-
-
-    # display the most common day of week
-
-
-    # display the most common start hour
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
-
-    print('\nCalculating The Most Popular Stations and Trip...\n')
-    start_time = time.time()
-
-    # display most commonly used start station
-
-
-    # display most commonly used end station
-
-
-    # display most frequent combination of start station and end station trip
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
-
-    print('\nCalculating Trip Duration...\n')
-    start_time = time.time()
-
-    # display total travel time
-
-
-    # display mean travel time
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def user_stats(df):
-    """Displays statistics on bikeshare users."""
-
-    print('\nCalculating User Stats...\n')
-    start_time = time.time()
-
-    # Display counts of user types
-
-
-    # Display counts of gender
-
-
-    # Display earliest, most recent, and most common year of birth
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
+      
 def main():
-    while True:
-        city, month, day = get_filters()
-        df = load_data(city, month, day)
+    # pick a city
+    city = get_city()
+    print('Great! We\'ll use %s.' % city)
+    
+    # load the file with input from above
+    city_data = get_city_data(cities[city])
+    
+    # parse datetime and column names
+    city_data['Start Time'] = pd.to_datetime(city_data['Start Time'])
+    city_data['End Time'] = pd.to_datetime(city_data['End Time'])  
+    city_data.columns = [x.strip().replace(' ', '_') for x in city_data.columns]
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+    # choose time period
+    period = get_time_period()
+    print('Filtering on time period: %s.' % period)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
-            break
-
+    if (period == 'month'):
+      month = get_month()
+      city_data['Start Time'].dt.month # BROKEN IDK WHY
+      print('Month selected: %s.' % month)
+    elif (period == 'day'):
+      day = get_day()
+      city_data[city_data['day'] == day] # BROKEN IDK WHY
+      print('Day selected: %s.' % day)
 
 if __name__ == "__main__":
 	main()
